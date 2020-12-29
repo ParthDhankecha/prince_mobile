bbda8f71826312ef95ca964b4c825e1c8bf57040
parthDhankecha <pmd3290@gmail.com>
2020-12-29 22:21:10 +0530

    Change of sms http

--- server/client/authentication.js
+++ server/client/authentication.js
@@ -367,18 +367,18 @@ router.post(
             // const agent = new https.Agent({
             //   rejectUnauthorized: false
             // });
-            // const axios = require('axios');
-            // axios.get(process.env.SMSHOST + encodeURI(path)).then(op => {
-            //   console.log(op.data)
-            //   // return res.status(200).json(op.data)
-            // })
-            //   .catch(error => {
-            //     console.log("error", error)
-            //     console.log(error.message)
-            //   });
-            http.get(process.env.SMSHOST + path, resp => {
-              console.log()
-            });
+            const axios = require('axios');
+            axios.get(process.env.SMSHOST + path).then(op => {
+              console.log(op.data)
+              // return res.status(200).json(op.data)
+            })
+              .catch(error => {
+                console.log("error", error)
+                console.log(error.message)
+              });
+            // http.get(process.env.SMSHOST + path, resp => {
+            //   console.log()
+            // });
             res
               .status(200)
               .json({ status: "1", message: "User registered successfully." });
