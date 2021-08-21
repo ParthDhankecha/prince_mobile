27717454bbdacab62c227bb4760103b2791db234
parthDhankecha <pmd3290@gmail.com>
2021-08-21 10:19:03 +0530

    Change of pickup code

--- server/routes/order.js
+++ server/routes/order.js
@@ -532,7 +532,7 @@ function bookShipment(order, res) {
           orderdata.total_weight = 0.05;
         }
         let shipment = {
-          PickupCode: "14",
+          PickupCode: "15",
           ShowDiffrenceSender: "Yes",
           SenderName: "Prince Mobile",
           SenderMobile: "9737156066",
